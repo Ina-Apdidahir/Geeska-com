f38b147a8941e41ad1026506cd2a49d5bc824651
Ina-Apdidahir <150722233+Ina-Apdidahir@users.noreply.github.com>
2024-08-24 12:26:40 +0300

    Updates

--- assets/browser-e1HZTtbU.js
+++ assets/browser-e1HZTtbU.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-DfSziGYj.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-DPXRF6je.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
