22355521cbef8c0ff22f280bfe4354d2a6deaf9d
Ina-Apdidahir <150722233+Ina-Apdidahir@users.noreply.github.com>
2024-08-27 21:04:49 +0300

    Updates

--- assets/browser-GqH0SQ7c.js
+++ assets/browser-GqH0SQ7c.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-DrPlIPG2.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-BuHX26Wh.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
